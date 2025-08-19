1d1eb4774c767ac1e446ead8663c698722d221e9
asifebrahim <jnesa38@gmail.com>
2025-08-20 02:42:36 +0530

    This is good

---
+++ src/main/java/org/example/testdeployment/TestDeploymentApplication.java
@@ -0,0 +1,13 @@
+package org.example.testdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/testdeployment/TestDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testdeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
